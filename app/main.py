bfbbc47c4555fd59e52f2edf08d9f6a6c9ec1fe2
Snir_DevOps <78410805+SnirM@users.noreply.github.com>
2026-01-18 21:24:06 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
-from app.routes import router
+from pydantic import BaseModel
 
-app = FastAPI(title="DevOps Portfolio API")
+app = FastAPI()
 
-app.include_router(router)
+class Person(BaseModel):
+    name: str
+    age: int
 
+DB = {}
 
 @app.get("/health")
-def health_check():
+def health():
     return {"status": "ok"}
+
+@app.get("/person")
+def get_people():
+    return DB
+
+@app.post("/person/{person_id}")
+def add_person(person_id: int, person: Person):
+    DB[person_id] = person.dict()
+    return {"message": "Person added", "data": DB[person_id]}
